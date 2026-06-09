850dd8173d8e53a2ee346b047faff4e7fe7e0078
johnhongannguyen <johhaneshongan@gmail.com>
2026-06-08 23:47:48 -0700

    repeat string conversion

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,10 +1,19 @@
-const repeatString = function(inputString,repeatTime) {
-    repeatTime = 0;
-    if(repeatTime <= 0)
-        return 'The input cannot be equal or less than 0'
-    inputString = '';
+// const repeatString = function(inputString,repeatTime) {
+//     repeatTime = 0;
+//     if(repeatTime <= 0)
+//         return 'The input cannot be equal or less than 0'
+//     inputString = '';
 
+// };
+
+
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
