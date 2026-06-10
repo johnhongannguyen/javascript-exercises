a6a565dfe815376b5df25554629f5ac35b118afc
johnhongannguyen <johhaneshongan@gmail.com>
2026-06-09 22:14:41 -0700

    adding input string function

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,19 +1,22 @@
-// const repeatString = function(inputString,repeatTime) {
-//     repeatTime = 0;
-//     if(repeatTime <= 0)
-//         return 'The input cannot be equal or less than 0'
-//     inputString = '';
+const repeatString = function(inputString,repeatTime) {
+    if(repeatTime <= 0)
+        return 'The input cannot be equal or less than 0'
+    let string = '';
+    for(let i = 0; i < repeatTime; i++){
+        string += inputString;
+    }
+    return string;
 
-// };
-
-
-const repeatString = function (word, times) {
-  if (times < 0) return "ERROR";
-  let string = "";
-  for (let i = 0; i < times; i++) {
-    string += word;
-  }
-  return string;
 };
+
+
+// const repeatString = function (word, times) {
+//   if (times < 0) return "ERROR";
+//   let string = "";
+//   for (let i = 0; i < times; i++) {
+//     string += word;
+//   }
+//   return string;
+// };
 // Do not edit below this line
 module.exports = repeatString;
