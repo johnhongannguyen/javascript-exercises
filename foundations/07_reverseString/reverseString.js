3585f21b4252b93e37e0e9ecf17a74d2c0878e7f
johnhongannguyen <johhaneshongan@gmail.com>
2026-06-18 22:36:07 -0700

    adding functions

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,8 +1,16 @@
 const reverseString = function(string) {
     if(string != typeof string)
         return 'Please input string'
+
     
 };
+
+// takes one number and returns that number + 7
+const add7 = function(number){
+    return number + 7
+}
+console.log(add7(10));
+// write a function called multiply that takes 2 numbers and returns their product
 
 // Do not edit below this line
 module.exports = reverseString;
